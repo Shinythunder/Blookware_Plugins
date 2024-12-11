04c49a9edf39eb204ce4ca124c23ec4185b18018
Shinythunder <130332657+Shinythunder@users.noreply.github.com>
2024-12-11 16:39:03 -0500

    Update test_plugin.js

--- test_plugin.js
+++ test_plugin.js
@@ -1,11 +1,1 @@
-(function() {
-    return {
-        name: "Sample Plugin",
-        description: "This is a test plugin.",
-        version: "1.0.0",
-        author: "Author Name",
-        action: function(enabled) {
-            console.log(`Plugin is now ${enabled ? 'enabled' : 'disabled'}`);
-        }
-    };
-})();
+console.log("testing")
